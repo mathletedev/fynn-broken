164dfe7b026a5b6b826f70e571122c47bc210886
mathletedev <mathletedev@gmail.com>
2021-11-05 23:38:26 -0700

    math eval

--- src/main/java/com/mathletedev/fynn/Bot.java
+++ src/main/java/com/mathletedev/fynn/Bot.java
@@ -1,14 +1,18 @@
 package com.mathletedev.fynn;
 
+import java.time.Instant;
 import javax.security.auth.login.LoginException;
 import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 public final class Bot extends ListenerAdapter {
 	public static void main(String[] args) throws LoginException {
@@ -21,14 +25,26 @@ public final class Bot extends ListenerAdapter {
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
 		Message message = event.getMessage();
+		User author = message.getAuthor();
+		if (!event.isFromGuild() || author.isBot())
+			return;
 
-		if (message.getContentRaw().equals("?ping")) {
-			MessageChannel channel = event.getChannel();
-			long time = System.currentTimeMillis();
+		String content = message.getContentRaw();
 
-			channel.sendMessage("Ping?").queue(res -> {
-				res.editMessageFormat("Pong!\n%d ms", System.currentTimeMillis() - time).queue();
-			});
+		Expression exp = new ExpressionBuilder(content).build();
+
+		if (!exp.validate().isValid()) {
+			message.reply("Unable to solve that expression...").queue();
+			return;
 		}
+
+		EmbedBuilder eb = new EmbedBuilder();
+		eb.setDescription("**Expression:**```" + content + "```\n**Result:**```"
+				+ String.valueOf(exp.evaluate()) + "```");
+		eb.setFooter(event.getMember().getNickname(), author.getAvatarUrl());
+		eb.setTimestamp(Instant.now());
+		eb.setColor(0x5865f2);
+
+		message.reply(eb.build()).queue();
 	}
 }
